037e660f0082131cd3713cc57aa263f17b7bea32
Chris Phillips <xposure@users.noreply.github.com>
2019-11-07 12:28:07 -0500

    moving code to correct projects

---
+++ src/Atma.Common/source/Atma/Entities/IComponentDataArray2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+namespace Atma.Entities
+{
+
+    public enum LockType
+    {
+        Read,
+        Write
+    }
+
+    public ref struct ComponentDataArrayReadLock
+    {
+        private readonly ReaderWriterLockSlim _rwLock;
+        public ComponentDataArrayReadLock(ReaderWriterLockSlim rwLock)
+        {
+            _rwLock = rwLock;
+        }
+
+        public void Dispose()
+        {
+            _rwLock.ExitReadLock();
+        }
+    }
+
+    public ref struct ComponentDataArrayWriteLock
+    {
+        private readonly ReaderWriterLockSlim _rwLock;
+        public ComponentDataArrayWriteLock(ReaderWriterLockSlim rwLock)
+        {
+            _rwLock = rwLock;
+        }
+
+        public void Dispose()
+        {
+            _rwLock.ExitWriteLock();
+        }
+    }
+
+    public interface IComponentDataArray2 : IDisposable
+    {
+        int ElementSize { get; }
+        int Length { get; }
+
+        ComponentDataArrayWriteLock AsSpan<T>(out Span<T> span) where T : unmanaged;
+        ComponentDataArrayReadLock AsReadOnlySpan<T>(out ReadOnlySpan<T> span) where T : unmanaged;
+        void Move<T>(int src, int dst) where T : unmanaged;
+    }
+}
--- src/Atma.Entities/source/Atma/Entities/ComponentDataArray2.cs
+++ src/Atma.Entities/source/Atma/Entities/ComponentDataArray2.cs
@@ -7,49 +7,6 @@
     using System.Threading;
     using Atma.Common;
 
-    public enum LockType
-    {
-        Read,
-        Write
-    }
-
-    public ref struct ComponentDataArrayReadLock
-    {
-        private readonly ReaderWriterLockSlim _rwLock;
-        internal ComponentDataArrayReadLock(ReaderWriterLockSlim rwLock)
-        {
-            _rwLock = rwLock;
-        }
-
-        public void Dispose()
-        {
-            _rwLock.ExitReadLock();
-        }
-    }
-
-    public ref struct ComponentDataArrayWriteLock
-    {
-        private readonly ReaderWriterLockSlim _rwLock;
-        internal ComponentDataArrayWriteLock(ReaderWriterLockSlim rwLock)
-        {
-            _rwLock = rwLock;
-        }
-
-        public void Dispose()
-        {
-            _rwLock.ExitWriteLock();
-        }
-    }
-
-    public interface IComponentDataArray2 : IDisposable
-    {
-        int ElementSize { get; }
-        int Length { get; }
-
-        ComponentDataArrayWriteLock AsSpan<T>(out Span<T> span) where T : unmanaged;
-        ComponentDataArrayReadLock AsReadOnlySpan<T>(out ReadOnlySpan<T> span) where T : unmanaged;
-        void Move<T>(int src, int dst) where T : unmanaged;
-    }
 
     public unsafe class ComponentDataArray2 : UnmanagedDispose, IComponentDataArray2
     {
